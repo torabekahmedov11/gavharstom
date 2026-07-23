ec57f11a225ce3ec809bba17572a6af337a52761
torabekahmedov11 <torabekahmedov11@gmail.com>
2026-07-23 18:34:23 +0500

    Remove compiled JS files from git tracking in api

--- api/_bot.d.ts
+++
@@ -1,5 +0,0 @@
-import { Bot } from 'grammy';
-import { PrismaClient } from '@prisma/client';
-export declare let tgBot: Bot | null;
-export declare const startBot: (prisma: PrismaClient) => void;
-//# sourceMappingURL=_bot.d.ts.map
